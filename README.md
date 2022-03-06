025815e948634aeda6d8b4f3adf80b98a900e10b
Zifeng Deng <oi_dzf@qq.com>
2022-03-06 23:44:45 +0800

    doc: Inital readme

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Project Name
+# co_context
+
+An io_context library aimed at low-latency io, based on liburingcxx.
+
+**co_context** 是一个**协程**异步多线程并发框架，主要瞄准性能（低延迟优先，高并发其次）。**co_context** 也致力于减轻用户的心智负担。让 C++ 初学者也能轻松写出高并发程序。
+
+目前 **co_context** 的设计仍处于极早期的混沌状态。No timeline and no roadmap yet.
+
+如果你正在寻找 C++ 异步的终极解决方案，且不在乎编程复杂度，我推荐你学习 **sender/receiver model**，而无需尝试协程。
+
+---
+
+<detail>
+
+# draft zone
+
+- 研究 liburingcxx 如何支持多生产者，多消费者并行（线程池中每个线程同时是 IO 生产者和消费者）
+- Coroutine 解决内联和动态内存分配问题
+- 表达式模板解决 task `&&` `||`。
+- 和 `std::execution` 能否兼容
+
+## 线程池实现
+
+- 一个内核线程 polling，一个主线程收集提交、收割推送I/O，其他固定 worker 线程，thread bind core
+- 节能模式：信号量表示允许的 idle worker 线程数量。低延模式：每个 worker 都 polling
+- 每个 worker 自带两条任务队列（一个sqe，一个cqe），固定长度，原子变量，cacheline友好。sqe放不下就放 std::queue，等有空位再放入共享cache。
+- 主线程cqe推送满了就切换到提交sqe
+- 主线程sqe提交满了就切换到推送cqe
+
+## 协程存在的问题
+
+### 弱点
+
+1. 除非编译器优化，每个协程都需要通过 `operator new` 来分配 frame：
+   - 动态内存分配可能引发性能问题；
+   - 在嵌入式或异构（例如 GPU）环境下，缺乏动态内存分配能力，难以工作。
+2. 除非编译器优化，协程的可定制点太多，需要大量间接调用/跳转（而不是内联），同样引起性能问题。
+   - 目前，编译器通常难以内联协程
+   - HALO 优化理论：[P0981R0](http://open-std.org/JTC1/SC22/WG21/docs/papers/2021/p2300r3.html#biblio-p0981r0)
+3. **动态分配**和**间接调用**的存在，导致协程暂时无法成为异步框架的最优方法。
+
+### 拆分子协程？
+
+- 出于性能考虑，不要将大协程拆分为几个小协程，因为会增加动态内存分配次数。
+  - 可以做 placement new 吗？
+
+### 与异步框架高度耦合
+
+1. 暂停和恢复都需要通过异步框架。
+2. 表达式模板的潜力不如 sender/receiver 模型：
+  - 协程是顺序/分支/循环结构，s/r是表达式。
+
+
+
+</detail>
---
+++ test/cat.cpp
@@ -0,0 +1,119 @@
+/*
+ *  A tester using liburingcxx.
+ *
+ *  Copyright (C) 2022 Zifeng Deng
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <fcntl.h>
+// #include <sys/stat.h>
+#include <iostream>
+#include <filesystem>
+#include "uring.hpp"
+
+constexpr unsigned BLOCK_SZ = 1024;
+
+struct FileInfo {
+    size_t size;
+    iovec iovecs[];
+};
+
+constexpr int countBlocks(size_t size, unsigned block_sz) noexcept {
+    return size / block_sz + ((size / block_sz * block_sz) != size);
+}
+
+void output(std::string_view s) noexcept {
+    for (char c : s) putchar(c);
+}
+
+using URing = liburingcxx::URing<0>;
+
+void submitReadRequest(
+    URing &ring, const std::filesystem::path path) {
+
+    // open the file
+    int file_fd = open(path.c_str(), O_RDONLY);
+    if (file_fd < 0) {
+        throw std::system_error{errno, std::system_category(), "open"};
+    }
+
+    // calculate the file size then malloc iovecs
+    const size_t file_size = std::filesystem::file_size(path);
+    const unsigned blocks = countBlocks(file_size, BLOCK_SZ);
+    FileInfo *fi = (FileInfo *)malloc(sizeof(*fi) + (blocks * sizeof(iovec)));
+    fi->size = file_size;
+
+    // malloc buffers (to later let the ring fill them asynchronously)
+    for (size_t rest = file_size, offset = 0, i = 0; rest != 0; ++i) {
+        size_t to_read = std::min<size_t>(rest, BLOCK_SZ);
+        fi->iovecs[i].iov_len = to_read;
+        if (posix_memalign(&fi->iovecs[i].iov_base, BLOCK_SZ, BLOCK_SZ) != 0) {
+            throw std::system_error{
+                errno, std::system_category(), "posix_memalign"};
+        }
+        rest -= to_read;
+    }
+
+    // submit the read request
+    liburingcxx::SQEntry &sqe = *ring.getSQEntry();
+    sqe.prepareReadv(file_fd, std::span{fi->iovecs, blocks}, 0)
+        .setData(reinterpret_cast<uint64_t>(fi));
+
+    // Must be called after any request (except for polling mode)
+    ring.submit();
+}
+
+
+
+void waitResultAndPrint(URing &ring) {
+    // get a result from the ring
+    liburingcxx::CQEntry &cqe = *ring.waitCQEntry();
+
+    // get the according data
+    FileInfo *fi = reinterpret_cast<FileInfo *>(cqe.getData());
+
+    // print the data to console
+    const int blocks = countBlocks(fi->size, BLOCK_SZ);
+    for (int i = 0; i < blocks; ++i) {
+        output({(char *)fi->iovecs[i].iov_base, fi->iovecs[i].iov_len});
+    }
+
+    // Must be called after consuming a cqe
+    ring.SeenCQEntry(&cqe);
+}
+
+int main(int argc, char *argv[]) {
+    using std::cout, std::endl, std::cerr;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [file name] <[file name] ...>\n", argv[0]);
+        return 1;
+    }
+
+    URing ring{4};
+
+    for (int i = 1; i < argc; ++i) {
+        try {
+            // put read request into the ring
+            submitReadRequest(ring, argv[i]);
+            // get string from the ring, and output to console
+            waitResultAndPrint(ring);
+        } catch (const std::system_error &e) {
+            cerr << e.what() << "\n" << e.code() << "\n";
+        } catch (const std::exception &e) { cerr << e.what() << '\n'; }
+    }
+
+    return 0;
+}
