fb64a8c9b18e8f13638e93446cf20f3f6f4cfac3
Zifeng Deng <oi_dzf@qq.com>
2022-04-09 19:32:23 +0800

    hint(sh): shorten format_all.sh

--- format_all.sh
+++ format_all.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 clang-format -i `find include/ -type f -name *.hpp`
-clang-format -i `find lib/ -type f -name *.cpp`
-clang-format -i `find example/ -type f -name *.cpp`
-clang-format -i `find test/ -type f -name *.cpp`
+clang-format -i `find lib/ example/ test/ -type f -name *.cpp`
