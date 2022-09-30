864f56bdb8e4d9e354bf755207ed917efb7cc0f8
Zifeng Deng <oi_dzf@qq.com>
2022-09-30 16:02:18 +0800

    feat(uring): syscall catch up with liburing

--- include/uring/syscall.hpp
+++ include/uring/syscall.hpp
@@ -1,90 +1,45 @@
 /* SPDX-License-Identifier: MIT */
-// #define _DEFAULT_SOURCE
+#pragma once
 
-/*
- * Will go away once libc support is there
- */
+#include <cerrno>
+#include <csignal>
+#include <cstdint>
 #include <unistd.h>
+#include <cstdbool>
+#include <sys/mman.h>
 #include <sys/syscall.h>
-#include <sys/uio.h>
-#include <signal.h>
+#include <sys/resource.h>
 
-#ifdef __alpha__
 /*
- * alpha and mips are exception, other architectures have
- * common numbers for new system calls.
+ * Don't put this below the #include "arch/$arch/syscall.h", that
+ * file may need it.
  */
-#ifndef __NR_io_uring_setup
-#define __NR_io_uring_setup 535
-#endif
-#ifndef __NR_io_uring_enter
-#define __NR_io_uring_enter 536
-#endif
-#ifndef __NR_io_uring_register
-#define __NR_io_uring_register 537
-#endif
-#elif defined __mips__
-#ifndef __NR_io_uring_setup
-#define __NR_io_uring_setup (__NR_Linux + 425)
-#endif
-#ifndef __NR_io_uring_enter
-#define __NR_io_uring_enter (__NR_Linux + 426)
-#endif
-#ifndef __NR_io_uring_register
-#define __NR_io_uring_register (__NR_Linux + 427)
-#endif
-#else /* !__alpha__ and !__mips__ */
-#ifndef __NR_io_uring_setup
-#define __NR_io_uring_setup 425
-#endif
-#ifndef __NR_io_uring_enter
-#define __NR_io_uring_enter 426
-#endif
-#ifndef __NR_io_uring_register
-#define __NR_io_uring_register 427
-#endif
-#endif
+struct io_uring_params;
 
-namespace liburingcxx {
+static inline void *ERR_PTR(intptr_t n) {
+    return (void *)n;
+}
 
-namespace detail {
+static inline intptr_t PTR_ERR(const void *ptr) {
+    return (intptr_t)ptr;
+}
 
-    inline int __sys_io_uring_register(
-        int fd, unsigned opcode, const void *arg, unsigned nr_args
-    ) {
-        return syscall(__NR_io_uring_register, fd, opcode, arg, nr_args);
-    }
+static inline bool IS_ERR(const void *ptr) {
+    return uintptr_t(ptr) >= uintptr_t(-4095UL);
+}
 
-    inline int
-    __sys_io_uring_setup(unsigned entries, struct io_uring_params *p) {
-        return syscall(__NR_io_uring_setup, entries, p);
-    }
-
-    inline int __sys_io_uring_enter2(
-        int fd,
-        unsigned to_submit,
-        unsigned min_complete,
-        unsigned flags,
-        sigset_t *sig,
-        int sz
-    ) {
-        return syscall(
-            __NR_io_uring_enter, fd, to_submit, min_complete, flags, sig, sz
-        );
-    }
-
-    inline int __sys_io_uring_enter(
-        int fd,
-        unsigned to_submit,
-        unsigned min_complete,
-        unsigned flags,
-        sigset_t *sig
-    ) {
-        return __sys_io_uring_enter2(
-            fd, to_submit, min_complete, flags, sig, _NSIG / 8
-        );
-    }
-
-} // namespace detail
-
-} // namespace liburingcxx
+#if defined(__x86_64__) || defined(__i386__)
+#include "arch/x86/syscall.h"
+#elif defined(__aarch64__)
+#include "arch/aarch64/syscall.h"
+#else
+/*
+ * We don't have native syscall wrappers
+ * for this arch. Must use libc!
+ */
+#ifdef CONFIG_NOLIBC
+#error "This arch doesn't support building liburing without libc"
+#endif
+/* libc syscall wrappers. */
+#include "arch/generic/syscall.h"
+#endif
