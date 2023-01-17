8480c0804cad0c865478ef3a63067a8574e63c08
Zifeng Deng <oi_dzf@qq.com>
2023-01-17 22:32:01 +0800

    feat(channel): support unbuffered channel<T, 0> perf(channel): replace std::queue by std::array perf(channel): improve channel<T, 1>

---
+++ example/channel.cpp
@@ -0,0 +1,50 @@
+#include "co_context/all.hpp"
+#include <type_traits>
+
+using namespace co_context;
+using namespace std;
+
+// unbuffered channel
+channel<std::string> chan;
+
+// buffered channel: more performace
+channel<std::string, 8> perf_chan;
+
+task<> produce(std::string tag) {
+    constexpr int repeat = 2;
+    for (;;) {
+        for (int i = 0; i < repeat; ++i) {
+            co_await chan.release(tag + ": fast produce");
+        }
+        for (int i = 0; i < repeat; ++i) {
+            co_await timeout(1s);
+            co_await chan.release(tag + ": slow produce");
+        }
+    }
+}
+
+task<> consume(std::string tag) {
+    for (;;) {
+        std::string str{co_await chan.acquire()};
+        printf("%s: %s\n", tag.c_str(), str.c_str());
+        co_await timeout(200ms);
+    }
+}
+
+int main() {
+    io_context ctx[6];
+    ctx[0].co_spawn(produce("p0"));
+    ctx[1].co_spawn(produce("p1"));
+    ctx[2].co_spawn(produce("p2"));
+
+    ctx[3].co_spawn(consume("c0"));
+    ctx[4].co_spawn(consume("c1"));
+    ctx[5].co_spawn(consume("c2"));
+
+    for (auto &c : ctx) {
+        c.start();
+    }
+
+    ctx[0].join();
+    return 0;
+}
--- include/co_context/co/channel.hpp
+++ include/co_context/co/channel.hpp
@@ -2,56 +2,225 @@
 
 #include "co_context/co/condition_variable.hpp"
 #include "co_context/co/semaphore.hpp"
+#include "co_context/detail/spsc_cursor.hpp"
+#include "co_context/task.hpp"
+#include "co_context/utility/mpl.hpp"
+#include <array>
+#include <concepts>
+#include <cstddef>
+#include <memory>
+#include <optional>
+#include <type_traits>
 
 namespace co_context {
-template<typename T, size_t capacity = 0>
+template<std::move_constructible T, size_t capacity = 0>
 class channel {
     static_assert(capacity != 0);
 
   public:
+    [[nodiscard]] size_t size() const noexcept { return m_size; }
+
+    [[nodiscard]] bool has_value() const noexcept { return !empty(); }
+
+    [[nodiscard]] bool empty() const noexcept { return m_size == 0; }
+
+    [[nodiscard]] bool full() const noexcept { return m_size == capacity; }
+
+    task<> drop() {
+        co_await m_mtx.lock();
+        if (empty()) {
+            co_await m_not_empty_cv.wait(m_mtx, [this] {
+                return !this->empty();
+            });
+        }
+        std::destroy_at(m_first);
+        pop_one();
+        m_mtx.unlock();
+        m_not_full_cv.notify_one();
+    }
+
     task<T> acquire() {
-        co_await mtx.lock();
-        co_await not_empty_cv.wait(mtx, [this] { return !this->buf.empty(); });
-        auto &&front = std::move(buf.front());
-        buf.pop();
-        mtx.unlock();
-        not_full_cv.notify_one();
-        co_return std::move(front);
+        co_await m_mtx.lock();
+        if (empty()) {
+            co_await m_not_empty_cv.wait(m_mtx, [this] {
+                return !this->empty();
+            });
+        }
+        T item{std::move(*m_first)};
+        std::destroy_at(m_first);
+        pop_one();
+        m_mtx.unlock();
+        m_not_full_cv.notify_one();
+        co_return std::move(item);
     }
 
-    task<> release(const T &x) {
-        co_await mtx.lock();
-        co_await not_full_cv.wait(mtx, [this] {
-            return this->buf.size() != capacity;
-        });
-        buf.push(x);
-        mtx.unlock();
-        not_empty_cv.notify_one();
-        co_return;
-    }
-
-    task<> release(T &&x) {
-        co_await mtx.lock();
-        co_await not_full_cv.wait(mtx, [this] {
-            return this->buf.size() != capacity;
-        });
-        buf.push(std::forward(x));
-        mtx.unlock();
-        not_empty_cv.notify_one();
-        co_return;
+    template<typename... Args>
+    task<> release(Args... args) {
+        co_await m_mtx.lock();
+        if (full()) {
+            co_await m_not_full_cv.wait(m_mtx, [this] {
+                return !this->full();
+            });
+        }
+        std::construct_at(m_last, std::forward<Args>(args)...);
+        push_one();
+        m_mtx.unlock();
+        m_not_empty_cv.notify_one();
     }
 
   private:
-    std::queue<T> buf;
-    condition_variable not_full_cv;
-    condition_variable not_empty_cv;
-    mutex mtx;
+    std::array<mpl::uninitialized<T>, capacity> m_buf;
+    T *m_first{reinterpret_cast<T *>(m_buf.data())};
+    T *m_last{reinterpret_cast<T *>(m_buf.data())};
+    size_t m_size{0};
+
+    co_context::condition_variable m_not_full_cv;
+    co_context::condition_variable m_not_empty_cv;
+    co_context::mutex m_mtx;
+
+    constexpr const T *buffer_start() const noexcept {
+        return reinterpret_cast<const T *>(m_buf.data());
+    }
+
+    constexpr const T *buffer_end() const noexcept {
+        return reinterpret_cast<const T *>(&(*m_buf.end()));
+    }
+
+    constexpr T *buffer_start() noexcept {
+        return reinterpret_cast<T *>(m_buf.data());
+    }
+
+    constexpr T *buffer_end() noexcept {
+        return reinterpret_cast<T *>(&(*m_buf.end()));
+    }
+
+    void pop_one() noexcept {
+        ++m_first;
+        --m_size;
+        if (m_first == buffer_end()) [[unlikely]] {
+            m_first = buffer_start();
+        }
+    }
+
+    void push_one() noexcept {
+        ++m_last;
+        ++m_size;
+        if (m_last == buffer_end()) [[unlikely]] {
+            m_last = buffer_start();
+        }
+    }
 };
 
-template<typename T>
-class channel<T, 0> {
+template<std::move_constructible T>
+class channel<T, 1UL> {
   public:
-    static constexpr int x = 0;
+    [[nodiscard]] size_t size() const noexcept { return m_buf.has_value(); }
+
+    [[nodiscard]] bool has_value() const noexcept { return m_buf.has_value(); }
+
+    [[nodiscard]] bool empty() const noexcept { return !has_value(); }
+
+    [[nodiscard]] bool full() const noexcept { return has_value(); }
+
+    task<> drop() {
+        co_await m_mtx.lock();
+        if (this->empty()) {
+            co_await m_not_empty_cv.wait(m_mtx, [this] {
+                return this->has_value();
+            });
+        }
+        m_buf = std::nullopt;
+        m_mtx.unlock();
+        m_not_full_cv.notify_one();
+    }
+
+    task<T> acquire() {
+        co_await m_mtx.lock();
+        if (this->empty()) {
+            co_await m_not_empty_cv.wait(m_mtx, [this] {
+                return this->has_value();
+            });
+        }
+        T item{std::move(m_buf.value())};
+        m_buf = std::nullopt;
+        m_mtx.unlock();
+        m_not_full_cv.notify_one();
+        co_return std::move(item);
+    }
+
+    template<typename... Args>
+    task<> release(Args... args) {
+        co_await m_mtx.lock();
+        if (this->full()) {
+            co_await m_not_full_cv.wait(m_mtx, [this] {
+                return !this->has_value();
+            });
+        }
+        m_buf.emplace(std::forward<Args>(args)...);
+        m_mtx.unlock();
+        m_not_empty_cv.notify_one();
+    }
+
+  private:
+    std::optional<T> m_buf{std::nullopt};
+    co_context::condition_variable m_not_full_cv;
+    co_context::condition_variable m_not_empty_cv;
+    co_context::mutex m_mtx;
+};
+
+template<std::move_constructible T>
+class channel<T, 0UL> {
+  public:
+    [[nodiscard]] consteval size_t size() const noexcept { return 0; }
+
+    [[nodiscard]] bool has_value() const noexcept { return m_has_sender; }
+
+    [[nodiscard]] bool empty() const noexcept { return m_receiver == nullptr; }
+
+    [[nodiscard]] bool full() const noexcept { return has_value(); }
+
+    task<T> acquire() {
+        std::optional<T> item{std::nullopt};
+        co_await m_acquire_mtx.lock();
+        co_await m_match_mtx.lock();
+        m_receiver = &item;
+        if (m_has_sender) {
+            m_match_cv.notify_one();
+        }
+        co_await m_match_cv.wait(m_match_mtx, [&item] {
+            return item.has_value();
+        });
+        m_has_sender = false;
+        m_match_mtx.unlock();
+        m_acquire_mtx.unlock();
+        co_return std::move(item.value());
+    }
+
+    template<typename... Args>
+    task<> release(Args... args) {
+        co_await m_release_mtx.lock();
+        co_await m_match_mtx.lock();
+        // wait until the receiver exists
+        if (m_receiver == nullptr) {
+            m_has_sender = true;
+            co_await m_match_cv.wait(m_match_mtx, [&receiver = m_receiver] {
+                return receiver != nullptr;
+            });
+        }
+        m_receiver->emplace(std::forward<Args>(args)...);
+        m_receiver = nullptr;
+        m_match_mtx.unlock();
+        m_match_cv.notify_one();
+        m_release_mtx.unlock();
+    }
+
+  private:
+    co_context::mutex m_acquire_mtx;
+    co_context::mutex m_release_mtx;
+    co_context::condition_variable m_match_cv;
+    co_context::mutex m_match_mtx;
+    std::optional<T> *m_receiver{nullptr};
+    bool m_has_sender = false;
 };
 
 } // namespace co_context
