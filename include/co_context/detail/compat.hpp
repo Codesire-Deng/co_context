7f4ed14f7d6e6c589a08777c09ec00788598fa73
Zifeng Deng <oi_dzf@qq.com>
2023-01-19 11:14:38 +0800

    refactor(compat): add `pause` instruction

--- include/co_context/detail/compat.hpp
+++ include/co_context/detail/compat.hpp
@@ -7,3 +7,11 @@
 #else
 #define CO_CONTEXT_ASSUME_ALIGNED(...)
 #endif
+
+#if defined(__clang__) || defined(__GNUC__) || defined(__GNUG__)
+#define CO_CONTEXT_PAUSE __builtin_ia32_pause
+#elif defined(_MSC_VER)
+#define CO_CONTEXT_PAUSE _mm_pause
+#else
+#define CO_CONTEXT_PAUSE __builtin_ia32_pause
+#endif
