06c36eeabac839cd402cc4be2671220b34ff15b4
Zifeng Deng <oi_dzf@qq.com>
2023-03-23 16:58:01 +0800

    refactor(uring): PATCHLEVEL was renamed to MINOR

--- include/uring/utility/kernel_version.hpp
+++ include/uring/utility/kernel_version.hpp
@@ -4,22 +4,22 @@
 #error Fail to detect kernel version. Please check CMakeLists.txt (at root level).
 #endif
 
-#ifndef LIBURINGCXX_KERNEL_VERSION_PATCHLEVEL
+#ifndef LIBURINGCXX_KERNEL_VERSION_MINOR
 #error Fail to detect kernel version. Please check CMakeLists.txt (at root level).
 #endif
 
 namespace liburingcxx {
 
-consteval bool is_kernel_reach(int major, int patchlevel) noexcept {
+consteval bool is_kernel_reach(int major, int minor) noexcept {
     if (LIBURINGCXX_KERNEL_VERSION_MAJOR != major) {
         return LIBURINGCXX_KERNEL_VERSION_MAJOR > major;
     }
-    return LIBURINGCXX_KERNEL_VERSION_PATCHLEVEL >= patchlevel;
+    return LIBURINGCXX_KERNEL_VERSION_MINOR >= minor;
 }
 
 } // namespace liburingcxx
 
-#define LIBURINGCXX_IS_KERNEL_REACH(major, patchlevel) \
+#define LIBURINGCXX_IS_KERNEL_REACH(major, minor) \
     ((LIBURINGCXX_KERNEL_VERSION_MAJOR > (major))      \
      || (LIBURINGCXX_KERNEL_VERSION_MAJOR == (major)   \
-         && LIBURINGCXX_KERNEL_VERSION_PATCHLEVEL >= (patchlevel)))
+         && LIBURINGCXX_KERNEL_VERSION_MINOR >= (minor)))
