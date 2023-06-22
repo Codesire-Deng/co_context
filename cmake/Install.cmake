95aa8aff29c097f8c2863c87c05a7ce59eb3450c
Zifeng Deng <oi_dzf@qq.com>
2023-06-22 20:33:00 +0800

    build(cmake): support install & find_package()

---
+++ cmake/Install.cmake
@@ -0,0 +1,40 @@
+include(GNUInstallDirs)
+
+install(TARGETS co_context
+        EXPORT co_context_targets
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+        INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+)
+
+install(DIRECTORY "${co_context_SOURCE_DIR}/include/co_context"
+        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+)
+
+install(DIRECTORY "${co_context_SOURCE_DIR}/include/uring"
+        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+)
+
+install(EXPORT co_context_targets
+        FILE co_context_targets.cmake
+        NAMESPACE co_context::
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/co_context
+)
+
+include(CMakePackageConfigHelpers)
+
+configure_package_config_file(${co_context_SOURCE_DIR}/cmake/templates/Config.cmake.in
+    "${CMAKE_CURRENT_BINARY_DIR}/co_context-config.cmake"
+    INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/co_context
+)
+
+write_basic_package_version_file(
+    "${CMAKE_CURRENT_BINARY_DIR}/co_context-config-version.cmake"
+    COMPATIBILITY SameMinorVersion)
+
+install(FILES
+        "${CMAKE_CURRENT_BINARY_DIR}/co_context-config.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/co_context-config-version.cmake"
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/co_context
+)
