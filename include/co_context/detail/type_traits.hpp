321cfac0a18feb7a22e8d4a2b70ec5be0f72a038
Zifeng Deng <oi_dzf@qq.com>
2023-09-10 01:05:02 +0800

    refactor(task): add type_traits.hpp

---
+++ include/co_context/detail/type_traits.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <type_traits> // IWYU pragma: export
+
+namespace co_context::detail {
+
+template<typename T>
+struct remove_rvalue_reference {
+    using type = T;
+};
+
+template<typename T>
+struct remove_rvalue_reference<T &&> {
+    using type = T;
+};
+
+template<typename T>
+using remove_rvalue_reference_t = typename remove_rvalue_reference<T>::type;
+
+template<typename Awaiter>
+using get_awaiter_result_t = decltype(std::declval<Awaiter>().await_resume());
+
+} // namespace co_context::detail
