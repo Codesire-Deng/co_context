e8e557c2671860270150569b2cb80d9e9bd9370e
Zifeng Deng <oi_dzf@qq.com>
2023-11-21 20:40:20 +0800

    feat(uring): compat kernel without `__kernel_timespec`. Thanks to Lawliet828 !

--- include/uring/compat.hpp
+++ include/uring/compat.hpp
@@ -1,6 +1,20 @@
 #pragma once
 
+#if __has_include(<linux/time_types.h>)
 #include <linux/time_types.h>
+/* <linux/time_types.h> is included above and not needed again */
+#define UAPI_LINUX_IO_URING_H_SKIP_LINUX_TIME_TYPES_H 1
+#else
+#include <cstdint>
+
+struct __kernel_timespec {
+	int64_t		tv_sec;
+	long long	tv_nsec;
+};
+
+/* <linux/time_types.h> is not available, so it can't be included */
+#define UAPI_LINUX_IO_URING_H_SKIP_LINUX_TIME_TYPES_H 1
+#endif
 
 #if __has_include(<linux/openat2.h>)
 #include <linux/openat2.h>
