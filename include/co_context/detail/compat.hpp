a6fba48f8677bc15c595b1f441c18a72c351441c
Severin Denisenko <81779013+SeverinDenisenko@users.noreply.github.com>
2026-08-08 23:40:23 +0000

    Add ARM support

--- include/co_context/detail/compat.hpp
+++ include/co_context/detail/compat.hpp
@@ -11,6 +11,8 @@
 #if defined(__clang__) || defined(__GNUC__) || defined(__GNUG__)
 #if defined(__i386__) || defined(__x86_64__)
 #define CO_CONTEXT_PAUSE() __builtin_ia32_pause()
+#elif defined(__aarch64__)
+#define CO_CONTEXT_PAUSE() __asm__ volatile("yield");
 #elif defined(__riscv)
 #define CO_CONTEXT_PAUSE() __asm__ volatile("fence iorw, iorw")
 #endif
